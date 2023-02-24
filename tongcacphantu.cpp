ede0374f622aad20a9f54f353b9c5c4f03332391
ThezFool <114967528+ThezFool@users.noreply.github.com>
2023-02-24 14:07:25 +0700

    Update tongcacphantu.cpp

--- tongcacphantu.cpp
+++ tongcacphantu.cpp
@@ -7,6 +7,7 @@ using namespace std;
 	{
 		int a[] ={12,5,61,6,18,54};
 		int n = sizeof(a)/sizeof(a[0]);
+		 cout <<"Kich thuoc mang a trong main(): " << sizeof(a)<< endl;
 		int s = sum(a,n);
 		cout << "Tong cac phan tu la: "<< s << endl;
 	}
@@ -14,10 +15,10 @@ using namespace std;
 	
 	 int sum(int a[],int n)
 	 {
+		  cout <<"Kich thuoc mang a trong sum(): " << sizeof(a)<< endl;
 	 	int s=0;
 	 	for (int i =0 ; i<n;i++ ){
 	 		s=s+a[i];
-	 		
 	 	}
 	 	return s;
 	 }
